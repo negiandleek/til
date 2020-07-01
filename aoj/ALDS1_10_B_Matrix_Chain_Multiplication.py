545c7e4305c271f23684f269187f1f55347b682d
youya66 <youya66@users.noreply.github.com>
2020-07-01 13:43:46 +0900

    Update ALDS1_10_B_Matrix_Chain_Multiplication.py

--- aoj/ALDS1_10_B_Matrix_Chain_Multiplication.py
+++ aoj/ALDS1_10_B_Matrix_Chain_Multiplication.py
@@ -15,3 +15,30 @@ for l in range(1, N):
         c0 = C[i+l]
         dp[i][i+l] = min(a0*C[j]*c0 + dp[i][j] + dp[j+1][i+l] for j in range(i, i+l))
 print(dp[0][N-1])
+
+# わからないので後回し
+n = int(input().rstrip())
+
+def calcMatrix(N, P):
+    matrix = [[None]*N for _ in range(N)]
+    for i in range(N):
+        matrix[i][i] = 0
+        
+    for l in range(1, N): #行列の数
+        for i in range(N - l): #行列の先頭
+            j = i + l #行列の後尾
+            for k in range(i, j): #組み合わせの区切りの位置
+                try:
+                    matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k+1][j] + P[i] + P[k] + P[j])   
+                except:
+                    print(i, j)
+                    print(*matrix)
+                
+
+arr = []            
+for _ in range(n):
+    *A, = input().rstrip().split()
+    arr.append(int(A[0] * int(A[1])))
+
+# print(arr)
+calcMatrix(n, arr)
