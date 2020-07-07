8c91f208f6add315161450d8ab33f4fbce13af50
youya66 <youya66@users.noreply.github.com>
2020-07-07 10:52:32 +0900

    Update ALDS1_11_D_Connected_Components.py

--- aoj/ALDS1_11_D_Connected_Components.py
+++ aoj/ALDS1_11_D_Connected_Components.py
@@ -1,13 +1,13 @@
 # TODO:
 n, m = map(int, input().rstrip().split())
 
-friends = [[0] * n]*n
-
+friends = [[0 for i in range(n)] for i in range(n)]
 for _ in range(m):
     a,b = map(int, input().rstrip().split())
     friends[a][b] = 1
-    
+
 ids = [None] * n
+
 def dfs(index, id):
     global ids
     ids[index] = id
@@ -15,7 +15,6 @@ def dfs(index, id):
     
     for i in range(n):
         if ids[i] == None and friends[index][i] == 1:
-            ids[i] = id
             dfs(i, id)
             
 for i in range(n):
@@ -23,7 +22,7 @@ for i in range(n):
         dfs(i, i + 1)
         
 q = int(input().rstrip())
-
+print(ids)
 for _ in range(q):
     a,b = map(int,input().rstrip().split())
     if ids[a] == ids[b]:
