5af0abcc6e41f1074e82acbb1006e48ce9076d8f
youya66 <17189098+negiandleek@users.noreply.github.com>
2020-07-22 06:55:02 +0900

    Update 7_exception.py

--- oreilly-typescript/7_exception.py
+++ oreilly-typescript/7_exception.py
@@ -1,4 +1,3 @@
-// TODO:
 class InvalidDateFormatError extends RangeError {}
 class DateIsInTheFutureError extends RangeError {}
 
@@ -50,12 +49,48 @@ function pparse(birthday: string): Date[] {
   return [date];
 }
 
-interface Option<T>{}
-class Some<T> implements Option<T>{
-  constructor(private value: T){}
+interface Option<T> {
+  flatMap<U>(f: (value: T) => None): None;
+  flatMap<U>(f: (value: T) => Option<U>): Option<U>;
+  getOrElse(value: T): T;
 }
 
+function option<T>(value: null | undefined): None;
+function option<T>(value: T): Some<T>;
+function option<T>(value: T): Option<T> {
+  if (value == null) {
+    return new None();
+  }
+  return new Some(value);
+}
+
+class Some<T> implements Option<T> {
+  constructor(private value: T) {}
+  flatMap<U>(f: (value: T) => None): None;
+  flatMap<U>(f: (value: T) => Some<U>): Some<U>;
+  flatMap<U>(f: (value: T) => Option<U>): Option<U> {
+    return f(this.value);
+  }
+  getOrElse(): T {
+    return this.value;
+  }
+}
+class None implements Option<never> {
+  flatMap(): None {
+    return this;
+  }
+  getOrElse<U>(value: U): U {
+    return value;
+  }
+}
+
+let result2 = option(6) // Some<number>
+.flatMap(n => option(n * 3)) // Some<number>
+.flatMap(n => new None) // None
+  .getOrElse(7) // 7
+
 aask()
-  .flatMap(parse)
-  .flatMap(date => _.toISOString())
-  .forEach((_) => console.info("Date is", _));
+  .flatMap(new Some(parse))
+  .flatMap(date => new Some(date.toISOString())) // Option<string>
+  .flatMap(date => new Some('Date is ' + date)) // Option<string>
+  .getOrElse('Error parsing date for some reason') // string
