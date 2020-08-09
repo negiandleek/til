339f3edeac184aaa6bd18da36f5fa36cd0c45aee
youya66 <youya66.official@gmail.com>
2020-08-09 22:25:44 +0900

    update 11_ambient.ts

--- oreilly-typescript/11_ambient.ts
+++ oreilly-typescript/11_ambient.ts
@@ -1,7 +1,17 @@
-declare module "react" {
+// アンビエント変数宣言
+declare let env: {
+  NODE_ENV: "development" | "production";
+};
+
+env.NODE_ENV;
+// NODE_ENV: "development" | "production"
+
+// アンビエントモジュール宣言
+declare module "module-name" {
   export type CustomType = string;
 }
 
-import React, { CustomType } from "react";
+import module from "module-name";
 
-const a: CustomType = "a";
+const a: module.CustomType = "a";
+// CustomType: string
