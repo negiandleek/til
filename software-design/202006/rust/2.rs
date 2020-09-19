df39f248aa935035fdb5b033fe95b0dcfeb4fd35
negiandleek <youya66.official@gmail.com>
2020-09-19 21:21:15 +0900

    update 2.rs

--- software-design/202006/rust/2.rs
+++ software-design/202006/rust/2.rs
@@ -26,9 +26,45 @@ fn main() {
     // 4
     println!("-----");
     {
+        let mut a = 1;
+        let b = a;
+        a = a + a;
         let s = "Hello".to_string();
         let t = s;
-        println!("{}", t);
-        println!("{}", s);
+        println!("{}, {}, {}", t, a, b);
+    }
+    
+    println!("-----");
+    {
+        fn myPrint<T: std::fmt::Display>(msg: &T){
+            println!("{}", msg);
+        }
+        fn exePrint(){
+            let s = "Hi".to_string();
+            myPrint(&s);
+            myPrint(&s);
+        }
+        exePrint();
+    }
+    
+    println!("-----");
+    {
+        fn exec(){
+          const N_THREAD : usize = 3;
+          let series_range = 0..30;
+          let add = 1;
+          
+          
+          let chunks = (0..N_THREAD)
+            .map(|ii| series_range.clone().skip(ii).step_by(N_THREAD));
+            
+          let handles : Vec<_> = chunks
+            .map(|vv| std::thread::spawn(move || {
+                vv.for_each(|nn| print!("{},", nn + add));
+            })).collect();
+            
+          handles.into_iter().for_each(|hh| hh.join().unwrap());
+        }
+        exec()
     }
 }
