342f55bb146944081de1a3dfee8fcbddbe3ed9b3
negiandleek <17189098+negiandleek@users.noreply.github.com>
2022-12-27 13:42:27 +0000

    today learned

--- ruby/プロを目指す人のためのRuby入門/2.rb
+++ ruby/プロを目指す人のためのRuby入門/2.rb
@@ -1,3 +1,14 @@
 pp nil.to_s
 pp 1.to_s
 pp true.to_s
+pp "---"
+pp 1.to_s
+pp 1.to_s()
+pp "---"
+x = {'japan' => 'yen', 'us' => 'dollar'}
+pp x
+pp "---"
+x,y = 100, 200, 300
+pp x, y
+z, zz = 400, 500,600
+pp(z, zz)
