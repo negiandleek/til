8f871424b87bde9597fe757088d11025b1520ae6
negiandleek <17189098+negiandleek@users.noreply.github.com>
2023-02-01 13:32:02 +0000

    til

--- ruby/プロを目指す人のためのRuby入門/2.rb
+++ ruby/プロを目指す人のためのRuby入門/2.rb
@@ -12,3 +12,56 @@ x,y = 100, 200, 300
 pp x, y
 z, zz = 400, 500,600
 pp(z, zz)
+pp "---"
+pp "a\nb"
+pp 'a\nb'
+pp "---"
+a = [1,2,3]
+b = {'hoge' => 'ほげ', 'fuga' => 'ふが'}
+pp a
+puts a
+pp b
+puts b
+pp "a\nb"
+puts "a\nb"
+a = b = 'value'
+a.upcase!
+pp a,b
+pp "---"
+pp 1 / 3
+pp 1.0 / 3.0
+pp "---"
+pp 1 + '2'.to_i
+pp 1.to_s + '2'
+pp (0.1r * 3r).to_f
+pp "---"
+country = 'hoge'
+country_local = 
+    if country == 'hoge'
+        'ほげ国'
+    elsif country == 'fuga'
+        'ふが国'
+    else
+        'ぴよ国'
+    end
+pp country_local
+pp "---"
+point = 2
+day = 3
+point *= 5 if day == 3
+pp point
+pp "ポイントは #{point} です"
+pp "---"
+pp %$hoge,fuga,piyo$
+pp %!hoge,fuga,piyo!
+puts <<TEXT
+hoge
+fuga
+piyo
+TEXT
+pp "---"
+pp 0b1111
+pp 0o10
+pp 0x10
+pp 2e3
+pp 2e-3
---
+++ ruby/プロを目指す人のためのRuby入門/2_fizz_buzz.rb
@@ -0,0 +1,17 @@
+def fizz_buzz(value)
+    if value % 15 == 0
+        'Fizz Buzz'
+    elsif value % 3 == 0
+        'Fizz'
+    elsif value % 5 == 0
+        'Buzz'
+    else
+        value.to_s
+    end
+end
+
+pp fizz_buzz(10)
+pp fizz_buzz(24)
+pp fizz_buzz(15)
+pp fizz_buzz(32)
+        
